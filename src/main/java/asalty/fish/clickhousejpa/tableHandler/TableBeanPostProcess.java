c3dfa39719b9300eb064d7cefd51675739ccdeb2
A-Salty-Fish <1309032417@qq.com>
2022-02-28 21:03:16 +0800

    feat：clickhouse表模块初始化

---
+++ src/main/java/asalty/fish/clickhousejpa/tableHandler/TableBeanPostProcess.java
@@ -0,0 +1,14 @@
+package asalty.fish.clickhousejpa.tableHandler;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: 容器启动后对clickhouse表的处理
+ * @date 2022/2/28 21:01
+ */
+
+public class TableBeanPostProcess {
+
+
+
+}
---
+++ src/main/java/asalty/fish/clickhousejpa/tableHandler/TableCreat.java
@@ -0,0 +1,11 @@
+package asalty.fish.clickhousejpa.tableHandler;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: 用于处理clickhouse表的创建
+ * @date 2022/2/28 21:02
+ */
+
+public class TableCreat {
+}
