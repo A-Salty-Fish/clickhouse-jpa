e6f18a753afba20d529b5b7a95b64feb62af47d3
A-Salty-Fish <1309032417@qq.com>
2022-03-02 10:34:26 +0800

    feat: 打包配置

--- src/main/java/asalty/fish/clickhousejpa/ClickhouseJpaApplication.java
+++ src/main/java/asalty/fish/clickhousejpa/ClickhouseJpaApplication.java
@@ -2,8 +2,12 @@ package asalty.fish.clickhousejpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+//@Configuration("ClickhouseJpaConfiguration")
+//@ComponentScan("asalty.fish.clickhousejpa")
 public class ClickhouseJpaApplication {
 
     public static void main(String[] args) {
