2aa2dd1e18bfcc916cb4eba38404846493d56eb9
A-Salty-Fish <1309032417@qq.com>
2022-03-31 16:10:38 +0800

    fix: annotation retention

--- src/main/java/asalty/fish/clickhousejpa/annotation/ClickHouseGeoColumns.java
+++ src/main/java/asalty/fish/clickhousejpa/annotation/ClickHouseGeoColumns.java
@@ -1,5 +1,12 @@
 package asalty.fish.clickhousejpa.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ClickHouseGeoColumns {
 
     String value() default "";
